e2ccb12a6d845df6d1b75f23b7f9780d0a22a434
bigboioct <43962896+bigboioct@users.noreply.github.com>
2023-05-04 16:08:08 -0400

    js changes

--- script.js
+++ script.js
@@ -4,9 +4,16 @@
 
     /* START OF NEW CODE 4/4/23 */
 
+    const addToChart = document.querySelector('#add_to_chart');
+    const clearChart = document.querySelector('#clear_chart');
     const url = new URL("https://trackapi.nutritionix.com/v2/search/instant");
-    const 
+    const params = 
 
+    addToChart.addEventListener('click', async (SubmitEvent) => {
+      console.log('added to chart');
+
+    })
+  }
 
   document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
   
