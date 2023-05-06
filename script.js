170c5b3324e4e25364062507989126973d476520
bigboioct <43962896+bigboioct@users.noreply.github.com>
2023-05-06 17:22:06 -0400

    store text in array for api query request

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 
   async function mainEvent() {
-    // the async keyword means we can make API requests
-
-    /* START OF NEW CODE 4/4/23 */
+ /* START OF NEW CODE 4/4/23 */
 
     const addToChart = document.querySelector('#add_to_chart');
     const clearChart = document.querySelector('#clear_chart');
     const url = new URL("https://trackapi.nutritionix.com/v2/search/instant");
-    const params = 
+    const input = document.querySelector("#input_Text");
+    const input_arr = [];
+    var final_input;
+
+    input.addEventListener('input', (event) => {
+      input_arr.push(document.getElementById("input_Text").value);
+      final_input = input_arr.slice(-1);
+      console.log(final_input);
+    })
 
     addToChart.addEventListener('click', async (SubmitEvent) => {
       console.log('added to chart');
+    })
+
+    clearChart.addEventListener('click', async (SubmitEvent) =>{
+      console.log('cleared chart');
 
     })
   }
