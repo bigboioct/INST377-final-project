e8ceced5dc3bf1d7f1ebcf35b7e47eeeb19d5917
bigboioct <43962896+bigboioct@users.noreply.github.com>
2023-05-07 16:00:54 -0400

    initial get request successful

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+function injectHTML(list) {
+  console.log("fired injectHTML");
+  const target = document.querySelector("#restaurant_list");
+  target.innerHTML = "";
+  list.forEach((item) => {
+    const str = `<li>${item.name}</li>`;
+    target.innerHTML += str;
+  });
+}
 
   async function mainEvent() {
- /* START OF NEW CODE 4/4/23 */
-
-    const addToChart = document.querySelector('#add_to_chart');
-    const clearChart = document.querySelector('#clear_chart');
-    const url = new URL("https://trackapi.nutritionix.com/v2/search/instant");
+    const mainForm = document.querySelector(".main_form")
+    const addToChart = document.querySelector("#add_to_chart");
+    const clearChart = document.querySelector("#clear_chart");
+    const addItem = document.querySelector("#add_Item")
     const input = document.querySelector("#input_Text");
     const input_arr = [];
     var final_input;
+    const url = "https://trackapi.nutritionix.com/v2/search/instant?query=";
 
     input.addEventListener('input', (event) => {
       input_arr.push(document.getElementById("input_Text").value);
       final_input = input_arr.slice(-1);
       console.log(final_input);
     })
+
+ addItem.addEventListener('click', async (SubmitEvent) => {
+      console.log("Loading Data");
+
+      const result = await fetch(url + final_input, {
+        headers: {
+          'x-app-id': 'a70a1a77',
+          'x-app-key': '85cb5f8ba0262142e4397b3393c105c4'
+        }
+      });
+
+      const data = await result.json()
+
+      console.log(data)
+    })
+    
 
     addToChart.addEventListener('click', async (SubmitEvent) => {
       console.log('added to chart');
