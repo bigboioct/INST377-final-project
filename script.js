692fd1255a5d87c82c88db5a2c5220851878daee
oali35 <123792995+oha35@users.noreply.github.com>
2023-05-09 15:45:38 -0400

    update script.js

--- script.js
+++ script.js
@@ -1,133 +1,58 @@
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
-  
-  function injectHTML(list) {
-    console.log("fired injectHTML");
-    const target = document.querySelector("#restaurant_list");
-    target.innerHTML = "";
-    list.forEach((item) => {
-      const str = `<li>${item.name}</li>`;
-      target.innerHTML += str;
-    });
-  }
-  
-  /* A quick filter that will return something based on a matching input */
-  function filterList(list, query) {
-    return list.filter((item) => {
-      const lowerCaseName = item.name.toLowerCase();
-      const lowerCaseQuery = query.toLowerCase();
-      return lowerCaseName.includes(lowerCaseQuery);
-    });
-  }
-  
-  function cutRestaurantList(list) {
-    console.log("fired cut list");
-    const range = [...Array(15).keys()];
-    return (newArray = range.map((item) => {
-      const index = getRandomIntInclusive(0, list.length - 1);
-      return list[index];
-    }));
-  }
+function injectHTML(list) {
+  console.log("fired injectHTML");
+  const target = document.querySelector("#restaurant_list");
+  target.innerHTML = "";
+  list.forEach((item) => {
+    const str = `<li>${item.name}</li>`;
+    target.innerHTML += str;
+  });
+}
 
-  function initMap(){
-    const carto = L.map('map').setView([38.98, -76.93], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(carto);
-    return carto;
-  }
-
-  function markerPlace(array,map){
-    console.log('array for markers', array);
-
-    map.eachLayer((layer) => {
-      if (layer instanceof L.Marker) {
-        layer.remove();
-      }
-    });
-
-
-    array.forEach((item) => {
-      console.log('markerPlace', item);
-      const {coordinates} = item.geocoded_column_1;
-
-      L.marker([coordinates[1], coordinates[0]]).addTo(map);
-    })
-  }
-  
   async function mainEvent() {
-    // the async keyword means we can make API requests
-    const mainForm = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
-    const loadDataButton = document.querySelector("#data_load");
-    const clearDataButton = document.querySelector("#data_clear");
-    const generateListButton = document.querySelector("#generate");
-    const textField = document.querySelector("#resto");
-  
-    const loadAnimation = document.querySelector("#data_load_animation");
-    loadAnimation.style.display = "none";
-    generateListButton.classList.add("hidden");
+    const mainForm = document.querySelector(".main_form")
+    const addToChart = document.querySelector("#add_to_chart");
+    const clearChart = document.querySelector("#clear_chart");
+    const addItem = document.querySelector("#add_Item")
+    const input = document.querySelector("#input_Text");
+    const input_arr = [];
+    var final_input;
+    const url = "https://trackapi.nutritionix.com/v2/search/instant?query=";
 
-/*     const carto = initMap(); */  
     const storedData = localStorage.getItem('storedData');
-    let parsedData = JSON.parse(storedData);
-  
-    if (parsedData?.length > 0) {
-      generateListButton.classList.remove("hidden");
-    }
-  
-    let currentList = [];
-  
-    /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-    loadDataButton.addEventListener("click", async (submitEvent) => {
-      // async has to be declared on every function that needs to "await" something
-      console.log("Loading data");
-      loadAnimation.style.display = "inline-block";
-  
-      const results = await fetch(
-        "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
-      );
-  
-      const storedList = await results.json();
-      localStorage.setItem('storedData', JSON.stringify(storedList));
-      parsedData = storedList;
-
-      if (parsedData?.length > 0) {
-        generateListButton.classList.remove("hidden");
-      }
-  
-      loadAnimation.style.display = "none";
-      // console.table(storedList);
-    });
-  
-  
-  
-    generateListButton.addEventListener("click", (event) => {
-      console.log("generate new list");
-      currentList = cutRestaurantList(parsedData);
-      console.log(currentList);
-      injectHTML(currentList);
-      markerPlace(currentList, carto);
-    });
-  
-    textField.addEventListener("input", (event) => {
-      console.log("input", event.target.value);
-      const newList = filterList(currentList, event.target.value);
-      console.log(newList);
-      injectHTML(newList);
-      markerPlace(newList, carto);
-    });
 
 
-    clearDataButton.addEventListener("click", (event) => {
-      console.log('clear browser data');
-      localStorage.clear();
-      console.log('localStorage Check', localStorage.getItem("storedData"))
+    input.addEventListener('input', (event) => {
+      input_arr.push(document.getElementById("input_Text").value);
+      final_input = input_arr.slice(-1);
+      console.log(final_input);
+    })
+
+ addItem.addEventListener('click', async (SubmitEvent) => {
+      console.log("Loading Data");
+
+      const result = await fetch(url + final_input, {
+        headers: {
+          'x-app-id': 'a70a1a77',
+          'x-app-key': '85cb5f8ba0262142e4397b3393c105c4'
+        }
+      });
+
+      const data = await result.json()
+      localStorage.setItem('storedData', JSON.stringify(data));
+      parsedData = data;
+      console.log(parsedData);
+    })
+    
+
+    addToChart.addEventListener('click', async (SubmitEvent) => {
+      console.log('added to chart');
+    })
+
+    clearChart.addEventListener('click', async (SubmitEvent) =>{
+      console.log('cleared chart');
+
     })
   }
-  
+
   document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
   
